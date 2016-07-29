c4aaebeae46e2b6f6fa1a0f415e74d3a0ade9698
Kelly Kearney <kakearney@gmail.com>
2016-07-29 15:13:06 -0700

    Added option to use prexisting map axis for plotromsrho.m

--- plotromsrho.m
+++ plotromsrho.m
@@ -1,20 +1,26 @@
-function hp = plotromsrho(Grd, data)
+function hp = plotromsrho(Grd, data, axflag)
 %PLOTROMSRHO Plots a rho-field variable from ROMS
 %
 % hp = plotromsrho(Grd, data)
+% hp = plotromsrho(Grd, data, flag)
 %
 % Input variables:
 %
 %   Grd:    grid file structure
 %
 %   data:   nxi x neta array
+%
+%   flag:   true to issue worldmap command on current axis, false to leave
+%           as is (must be map axis)
 
-% h = plotgrid('setup', cell(1), [],[], 'mar', 0.02);
-% h.fig.Color = 'w';
+if nargin < 3
+    axflag = true;
+end
 
-latlim = minmax(Grd.lat_rho);
-lonlim = minmax(Grd.lon_rho);
-
-worldmap(latlim, lonlim);
+if axflag
+    latlim = minmax(Grd.lat_rho);
+    lonlim = minmax(Grd.lon_rho);
+    worldmap(latlim, lonlim);
+end
+    
 hp = pcolorm(Grd.lat_psi, Grd.lon_psi, padarray(data(2:end-1,2:end-1), [1 1], NaN, 'post'));
-% setm(ax, 'flinewidth', 1);
